1deb7bce881ebe8dc57e91a2e71f7716479c673b
Chaeeun1021 <dmslv1021@naver.com>
2024-05-10 14:40:20 +0900

    설문

---
+++ web8_server/src/main/java/hello/hellospring/domain/SurveyResult.java
@@ -0,0 +1,30 @@
+package hello.hellospring.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "survey_result")
+public class SurveyResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "count")
+    private int count;
+
+    public void setGenre(String genre) {
+    }
+
+    public void setCount(int i) {
+    }
+
+    public int getCount() {
+        return 0;
+    }
+
+    // 생성자, 게터, 세터
+}
---
+++ web8_server/src/main/java/hello/hellospring/service/SurveyResultService.java
@@ -0,0 +1,38 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.SurveyResult;
+import hello.hellospring.repository.SurveyResultRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class SurveyResultService {
+
+    @Autowired
+    private SurveyResultRepository surveyResultRepository;
+
+    @Transactional
+    public void updateGenreCount(String genre) {
+        SurveyResult surveyResult = surveyResultRepository.findByGenre(genre);
+        if (surveyResult != null) {
+            surveyResult.setCount(surveyResult.getCount() + 1);
+            surveyResultRepository.save(surveyResult);
+        } else {
+            // 새로운 장르일 경우, 새로운 엔터티 생성 후 저장
+            surveyResult = new SurveyResult();
+            surveyResult.setGenre(genre);
+            surveyResult.setCount(1);
+            surveyResultRepository.save(surveyResult);
+        }
+    }
+    @Transactional(readOnly = true)
+    public int getTotalSurveyCount() {
+        Iterable<SurveyResult> allSurveyResults = surveyResultRepository.findAll();
+        int totalSurveyCount = 0;
+        for (SurveyResult result : allSurveyResults) {
+            totalSurveyCount += result.getCount();
+        }
+        return totalSurveyCount;
+    }
+}
