12e19b3d2d37e1c04fefb3289ac110d3e2eee2ab
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-05-15 21:01:44 +0900

    음악동시재생 방지

--- music/src/Pages/FirstTest/FirstPage.js
+++ music/src/Pages/FirstTest/FirstPage.js
@@ -14,10 +14,21 @@ import "./FirstPage.css"
 const FirstPage = () => {
 
     const [isPlaying, setIsPlaying] = useState(false);
-    const audioRef = useRef(new Audio('./ex.mp3'));
+    const audioRef = useRef([
+        new Audio('./ex.mp3'),
+        new Audio('./trap.mp3'),
+    ]);
 
-    const playMusic = () => {
-        const audio = audioRef.current;
+    const playMusic = (index) => {
+        const audio = audioRef.current[index];
+
+        // 이전에 재생 중이던 모든 오디오 일시 정지 
+        audioRef.current.forEach((audio,i)=>{
+            if (i !== index && !audio.paused){
+                audio.pause();
+                audio.currentTime = 0;
+            }
+        })
 
         if (audio.paused) {
         audio.play(); 
@@ -26,7 +37,6 @@ const FirstPage = () => {
         }
         
         setIsPlaying(!isPlaying);
-        // audio.currenTime = 0 // 클릭하면 다시 처음으로 
     };
 
     
@@ -38,27 +48,27 @@ const FirstPage = () => {
                 <Progress value={8} size='md' colorScheme='purple'/>
             </div>
             <div className="num1">
-                <img onClick={playMusic} src="./sound1.png" alt='Sound Icon'/>
+                <img onClick={() => playMusic(0)} src="./sound1.png" alt='Sound Icon'/>
                 <h3><span>1.</span><div class="q1">1번 사운드가 당신의 마음에 드나요?</div></h3>
                 <PopBox id="Pop1"/>
             </div>
             <div className="num1">
-                <img onClick={playMusic} src="./sound1.png" alt='Sound Icon'/>
+                <img onClick={() => playMusic(1)} src="./sound1.png" alt='Sound Icon'/>
                 <h3><span>2.</span><div class="q1">2번 사운드가 당신의 마음에 드나요?</div></h3>
                 <PopBox id="Pop2"/>
             </div>
             <div className="num1">
-                <img onClick={playMusic} src="./sound1.png" alt='Sound Icon'/>
+                <img onClick={() => playMusic(1)} src="./sound1.png" alt='Sound Icon'/>
                 <h3><span>3.</span><div class="q1">3번 사운드가 당신의 마음에 드나요?</div></h3>
                 <HipBox id="Hip1"/>
             </div>
             <div className="num1">
-                <img onClick={playMusic} src="./sound1.png" alt='Sound Icon'/>
+                <img onClick={() => playMusic(1)} src="./sound1.png" alt='Sound Icon'/>
                 <h3><span>4.</span><div class="q1">4번 사운드가 당신의 마음에 드나요?</div></h3>
                 <HipBox id="Hip2"/>
             </div>
             <div className="num1">
-                <img onClick={playMusic} src="./sound1.png" alt='Sound Icon'/>
+                <img onClick={() => playMusic(1)} src="./sound1.png" alt='Sound Icon'/>
                 <h3><span>5.</span><div class="q1">5번 사운드가 당신의 마음에 드나요?</div></h3>
                 <RockBox id="Rock1"/>
             </div>
--- music/src/Pages/FirstTest/FirstPage1.js
+++ music/src/Pages/FirstTest/FirstPage1.js
@@ -12,10 +12,21 @@ import "./FirstPage.css";
 const FirstPage1 = () => {
 
     const [isPlaying, setIsPlaying] = useState(false);
-    const audioRef = useRef(new Audio('./trap.mp3'));
+    const audioRef = useRef([
+        new Audio('./ex.mp3'),
+        new Audio('./trap.mp3'),
+    ]);
 
-    const playMusic = () => {
-        const audio = audioRef.current;
+    const playMusic = (index) => {
+        const audio = audioRef.current[index];
+
+        // 이전에 재생 중이던 모든 오디오 일시 정지 
+        audioRef.current.forEach((audio,i)=>{
+            if (i !== index && !audio.paused){
+                audio.pause();
+                audio.currentTime = 0;
+            }
+        })
 
         if (audio.paused) {
         audio.play(); 
@@ -24,7 +35,6 @@ const FirstPage1 = () => {
         }
         
         setIsPlaying(!isPlaying);
-        // audio.currenTime = 0 // 클릭하면 다시 처음으로 
     };
 
     return (
@@ -34,27 +44,27 @@ const FirstPage1 = () => {
                 <Progress value={16} size='md' colorScheme='purple' />       
             </div>
             <div className="num1">
-                <img onClick={playMusic} src="./sound1.png" alt='Sound Icon'/>
+                <img onClick={() => playMusic(0)} src="./sound1.png" alt='Sound Icon'/>
                 <h3><span>6.</span><div class="q1">6번 사운드가 당신의 마음에 드나요?</div></h3>
                 <RockBox id="Rock2"/>
             </div>
             <div className="num1">
-                <img onClick={playMusic} src="./sound1.png" alt='Sound Icon'/>
+                <img onClick={() => playMusic(1)} src="./sound1.png" alt='Sound Icon'/>
                 <h3><span>7.</span><div class="q1">7번 사운드가 당신의 마음에 드나요?</div></h3>
                 <RbBox id="RB1"/>
             </div>
             <div className="num1">
-                <img onClick={playMusic} src="./sound1.png" alt='Sound Icon'/>
+                <img onClick={() => playMusic(1)} src="./sound1.png" alt='Sound Icon'/>
                 <h3><span>8.</span><div class="q1">8번 사운드가 당신의 마음에 드나요?</div></h3>
                 <RbBox id="RB2"/>
             </div>
             <div className="num1">
-                <img onClick={playMusic} src="./sound1.png" alt='Sound Icon'/>
+                <img onClick={() => playMusic(1)} src="./sound1.png" alt='Sound Icon'/>
                 <h3><span>9.</span><div class="q1">9번 사운드가 당신의 마음에 드나요?</div></h3>
                 <JazzBox id="Jazz1"/>
             </div>
             <div className="num1">
-                <img onClick={playMusic} src="./sound1.png" alt='Sound Icon'/>
+                <img onClick={() => playMusic(1)} src="./sound1.png" alt='Sound Icon'/>
                 <h3><span>10.</span><div class="q1">10번 사운드가 당신의 마음에 드나요?</div></h3>
                 <JazzBox id="Jazz2"/>
             </div>
