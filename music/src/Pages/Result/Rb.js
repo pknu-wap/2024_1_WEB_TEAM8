b8362649cc427c6173f273d5c252db98dec609f8
이찬이 <158177151+chanxami@users.noreply.github.com>
2024-05-17 16:22:41 +0900

    R&B 결과 페이지 디자인 적용

--- music/src/Pages/Result/Rb.js
+++ music/src/Pages/Result/Rb.js
@@ -47,25 +47,74 @@ const Rb = () => {
 
     return (
         <ChakraProvider>
-        <div className="Top">
-            <h2>당신은 '알앤비'을 좋아하실겁니다.</h2>
-        </div>
-        <div className="Playlist">
-            <h3>이 노래를 들어보세요!!</h3>
-            <h3>- 추천 플레이리스트 바로가기 -</h3>
-        </div>
-        (플레이리스트 링크)
-        <hr/>
-        <div className="RB">
+        <div className="body">
+        <br/><br/><br/><br/><br/>
 
+        <div className="Top">
+            <h3>당신의 음악 DNA는</h3>
+            <h2>'<span>R&B</span>'에 가깝군요</h2>
+        </div>    
+
+        <div className="RB">
+        <span>R&B</span><br/>
+        대충 r&b 설명하기와ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
         </div>
+
+        <div class="singer">
+        <table>
+        <tr>
+        <td><span>NeoRB</span><br/>< img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="D'Angelo"/>D'Angelo</td>
+        <td><span>ComRB</span><br/><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="SZA" />SZA<br/></td>
+        <td><span>PunkRB</span><br/><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Michael Jackson" />Michael Jackson<br/></td>
+        <td><span>SoulRB</span><br/><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Stevie Wonder" />Stevie Wonder<br/></td>
+        <td><span>AlterRB</span><br/><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Frank ocean" />Frank ocean</td> 
+        </tr>
+        </table>
+        </div>
+        
+        <div className="Playlist">
+            <h2>이 노래는 어때요?</h2>
+            <h3>추천 플레이리스트 보기</h3>
+        </div>
+
+        <div class="musictable">
+    <table>
+        <tr>
+        <hr/><td>
+            <img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="D'Angelo"/>D'Angel<br/>오둥1
+            <div class="play1"><img src="sound1.png"/><span>play</span></div></td>
+            <hr/>
         <hr/>
-        <div className="caution">
-            <h3>수많은 장르 중 하나의 장르를 선별하는 테스트이므로</h3>
-            <h3>개발자의 주관적인 판단, 생각이 들어있습니다.</h3>
-            <h3>결과는 언제나 시간에 따라 달라질 수 있으며</h3>
-            <h3>절대적이지 않음을 말씀드립니다.</h3>
+        </tr>
+        <tr>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="SZA" />SZA<br/>오둥2
+            <div class="play1"><img src="/sound1.png"/><span>play</span></div>
+            </td><hr/>
+        </tr>
+        <tr>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Michael Jackson" />Michael Jackson<br/>오둥3
+            <div class="play"><img src="sound1.png"/><span>play</span></div>
+            </td><hr/>
+        </tr>
+        <tr>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Stevie Wonder" />Stevie Wonder<br/>오둥4
+            <div class="play"><img src="sound1.png"/><span>play</span></div>
+            </td><hr/>
+        </tr>
+        <tr>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Frank ocean" />Frank ocean<br/>오둥5
+            <div class="play"><img src="sound1.png"/>play</div>
+            </td><hr/> 
+        </tr>
+    </table>
+    </div>
+
+
+
+        <div class="graph">
+        <h2>현재 <span>나의 음악 DNA</span>분포도</h2>
         </div>
+        
         <VStack
             className="Stack"
             divider={<StackDivider borderColor='gray.300' />}
@@ -118,7 +167,13 @@ const Rb = () => {
                 </div>
             </Box>
         </VStack>
-        
+        <div className="caution">
+            <h3>수많은 장르 중 하나의 장르를 선별하는 테스트이므로</h3>
+            <h3>개발자의 주관적인 판단, 생각이 들어있습니다.</h3>
+            <h3>결과는 언제나 시간에 따라 달라질 수 있으며</h3>
+            <h3>절대적이지 않음을 말씀드립니다.</h3>
+        </div>
+        </div>
         </ChakraProvider>
     );
 }
