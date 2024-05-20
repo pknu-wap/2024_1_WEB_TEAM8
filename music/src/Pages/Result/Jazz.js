8f26fb8e42cb37a66b9e7b23089fa6544cddb7a3
이찬이 <158177151+chanxami@users.noreply.github.com>
2024-05-20 14:09:56 +0900

    jazz 결과내용

--- music/src/Pages/Result/Jazz.js
+++ music/src/Pages/Result/Jazz.js
@@ -64,27 +64,73 @@ const Jazz = () => {
 
     return (
         <ChakraProvider>
-        <div className="Top">
-            <h2>당신의 음악 DNA는 </h2>
-            <p><span>재즈에 속한 하위장르 중 {maxGenre}에 가깝군요</span>에 가깝군요</p>
-        </div>
-        <div className="Playlist">
-            <h3>이 노래를 들어보세요!!</h3>
-            <h3>- 추천 플레이리스트 바로가기 -</h3>
-        </div>
-        (플레이리스트 링크)
-        <hr/>
-        <div className="RB">
+               <div className="body">
+        <br/><br/><br/><br/><br/>
 
+        <div className="Top">
+            <h3>당신의 음악 DNA는</h3>
+            <h2>'<span>재즈</span>' 에 가깝군요</h2>
+        </div>    
+        
+        <div class="singer">
+        <table>
+        <tr>
+        <td><span>LatinJazz</span><br/>< img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Stan Getz"/>Stan Getz</td>
+        <td><span>SwingJazz</span><br/><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Benny Goodman" />Benny Goodman<br/></td>
+        <td><span>SoulJazz</span><br/><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Jimmy smith" />Jimmy smith<br/></td>
+        <td><span>FreeJazz</span><br/><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Ornette Coleman" />Ornette Coleman<br/></td>
+        <td><span>BibobJazz</span><br/><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Charlie Parker" />Charlie Parker</td> 
+        </tr>
+        </table>
         </div>
+
+        <div className="ques">
+            <h2>이 노래는 어떠신가요?</h2>
+            <h3>추천 플레이리스트 보기</h3>
+        </div>
+
+    <div class="musictable">
+       <table>
+        <tr>
+        <hr/><td>
+            <img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="D'Angelo"/>
+            <div id="song">The Girl From Ipanema<br/><span>Stan Getz</span></div>
+            <div class="play"><img src="sound1.png" id="m1"/><span>play</span></div></td>
+            <hr/>
         <hr/>
-        <div className="caution">
-            <h3>수많은 장르 중 하나의 장르를 선별하는 테스트이므로</h3>
-            <h3>개발자의 주관적인 판단, 생각이 들어있습니다.</h3>
-            <h3>결과는 언제나 시간에 따라 달라질 수 있으며</h3>
-            <h3>절대적이지 않음을 말씀드립니다.</h3>
+        </tr>
+        <tr>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="SZA" />
+            <div id="song">Sing Sing Sing<br/><span>Benny Goodman</span></div>
+            <div class="play"><img src="/sound1.png" id="m2"/><span>play</span></div>
+            </td><hr/>
+        </tr>
+        <tr>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Michael Jackson" />
+            <div id="song">Midnight Special<br/><span>Jimmy smith</span></div>
+            <div class="play"><img src="sound1.png" id="m3"/><span>play</span></div>
+            </td><hr/>
+        </tr>
+        <tr>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Stevie Wonder" />
+            <div id="song"> Lonely Woman<br/><span>Ornette Coleman</span></div>
+            <div class="play"><img src="sound1.png" id="m4"/><span>play</span></div>
+            </td><hr/>
+        </tr>
+        <tr>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Frank ocean" />
+            <div id="song">Confirmation<br/><span>Charlie Parker</span></div>
+            <div class="play"><img src="sound1.png" id="m5"/><span>play</span></div>
+            </td><hr/> 
+        </tr>
+    </table>
+    </div>
+
+        <div class="graph">
+        <h2>현재 <span>나의 음악 DNA</span>분포도</h2>
         </div>
-        <VStack
+
+    <VStack
             className="Stack"
             divider={<StackDivider borderColor='gray.300' />}
             spacing={3}
@@ -136,6 +182,11 @@ const Jazz = () => {
                 </div>
             </Box>
         </VStack>
+        <div className="caution">
+            <h3>수많은 장르 중 하나의 장르를 선별하는 테스트이므로 개발자의 주관적인 판단, 생각이 들어있습니다.</h3>
+            <h3>결과는 언제나 시간에 따라 달라질 수 있으며 절대적이지 않음을 말씀드립니다.</h3>
+        </div>
+    </div>
         </ChakraProvider>
     );
 }
