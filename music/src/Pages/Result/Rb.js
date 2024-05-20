25a51c87735547f735175cde007c5c6deafea94a
이찬이 <158177151+chanxami@users.noreply.github.com>
2024-05-20 12:42:43 +0900

    결과 R&B

--- music/src/Pages/Result/Rb.js
+++ music/src/Pages/Result/Rb.js
@@ -52,13 +52,8 @@ const Rb = () => {
 
         <div className="Top">
             <h3>당신의 음악 DNA는</h3>
-            <h2>'<span>R&B</span>'에 가깝군요</h2>
+            <h2>'<span>R&B</span>' 에 가깝군요</h2>
         </div>    
-
-        <div className="RB">
-        <span>R&B</span><br/>
-        대충 r&b 설명하기와ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
-        </div>
 
         <div class="singer">
         <table>
@@ -72,8 +67,8 @@ const Rb = () => {
         </table>
         </div>
         
-        <div className="Playlist">
-            <h2>이 노래는 어때요?</h2>
+        <div className="ques">
+            <h2>이 노래는 어떠신가요?</h2>
             <h3>추천 플레이리스트 보기</h3>
         </div>
 
@@ -81,29 +76,35 @@ const Rb = () => {
     <table>
         <tr>
         <hr/><td>
-            <img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="D'Angelo"/>D'Angel<br/>오둥1
-            <div class="play1"><img src="sound1.png"/><span>play</span></div></td>
+            <img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="D'Angelo"/>
+            <div id="song">1번곡 이름<br/><span>D'Angelo</span></div>
+            <img src="19.png" id="prohibit"/>
+            <div class="play"><img src="sound1.png" id="m1"/><span>play</span></div></td>
             <hr/>
         <hr/>
         </tr>
         <tr>
-            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="SZA" />SZA<br/>오둥2
-            <div class="play1"><img src="/sound1.png"/><span>play</span></div>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="SZA" />
+            <div id="song">2번곡의 이름<br/><span>SZA</span></div>
+            <div class="play"><img src="/sound1.png" id="m2"/><span>play</span></div>
             </td><hr/>
         </tr>
         <tr>
-            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Michael Jackson" />Michael Jackson<br/>오둥3
-            <div class="play"><img src="sound1.png"/><span>play</span></div>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Michael Jackson" />
+            <div id="song">3번곡 이름<br/><span>Michael Jackson</span></div>
+            <div class="play"><img src="sound1.png" id="m3"/><span>play</span></div>
             </td><hr/>
         </tr>
         <tr>
-            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Stevie Wonder" />Stevie Wonder<br/>오둥4
-            <div class="play"><img src="sound1.png"/><span>play</span></div>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Stevie Wonder" />
+            <div id="song">4번곡 이름<br/><span>Stevie Wonder</span></div>
+            <div class="play"><img src="sound1.png" id="m4"/><span>play</span></div>
             </td><hr/>
         </tr>
         <tr>
-            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Frank ocean" />Frank ocean<br/>오둥5
-            <div class="play"><img src="sound1.png"/>play</div>
+            <td><img src="https://item.kakaocdn.net/do/0c5d8e3d0f5a44781ef255a1735482b19f17e489affba0627eb1eb39695f93dd" alt="Frank ocean" />
+            <div id="song">5번곡 이름<br/><span>Frank ocean</span></div>
+            <div class="play"><img src="sound1.png" id="m5"/><span>play</span></div>
             </td><hr/> 
         </tr>
     </table>
@@ -168,10 +169,8 @@ const Rb = () => {
             </Box>
         </VStack>
         <div className="caution">
-            <h3>수많은 장르 중 하나의 장르를 선별하는 테스트이므로</h3>
-            <h3>개발자의 주관적인 판단, 생각이 들어있습니다.</h3>
-            <h3>결과는 언제나 시간에 따라 달라질 수 있으며</h3>
-            <h3>절대적이지 않음을 말씀드립니다.</h3>
+            <h3>수많은 장르 중 하나의 장르를 선별하는 테스트이므로 개발자의 주관적인 판단, 생각이 들어있습니다.</h3>
+            <h3>결과는 언제나 시간에 따라 달라질 수 있으며 절대적이지 않음을 말씀드립니다.</h3>
         </div>
         </div>
         </ChakraProvider>
