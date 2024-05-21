54b9289aefe45bd143d09ec390289cbdcc0b8b75
이찬이 <158177151+chanxami@users.noreply.github.com>
2024-05-21 13:03:54 +0900

    rock 결과 내용

--- music/src/Pages/Result/Rock.js
+++ music/src/Pages/Result/Rock.js
@@ -49,25 +49,71 @@ const Rock = () => {
 
     return (
         <ChakraProvider>
-        <div className="Top">
-            <h2>당신은 '락'을 좋아하실겁니다.</h2>
-        </div>
-        <div className="Playlist">
-            <h3>이 노래를 들어보세요!!</h3>
-            <h3>- 추천 플레이리스트 바로가기 -</h3>
-        </div>
-        (플레이리스트 링크)
-        <hr/>
-        <div className="RB">
+       <div className="body">
+        <br/><br/><br/><br/><br/>
 
+        <div className="Top">
+            <h3>당신의 음악 DNA는</h3>
+            <h2>'<span>Rock</span>' 에 가깝군요</h2>
+        </div>    
+
+        <div class="singer">
+        <table>
+        <tr>
+        <td><span>ProRock</span><br/><br/>< img src="/rock_singer1.png" alt="Yes"/>Yes</td>
+        <td><span>HeavyRock</span><br/><br/><img src="/rock_singer2.png" alt="Metallica" />Metallica<br/></td>
+        <td><span>AlterRock</span><br/><br/><img src="/rock_singer3.png" alt="Nirvana" />Nirvana<br/></td>
+        <td><span>PunkRock</span><br/><br/><img src="/rock_singer4.png" alt="Sex pistols" />Sex pistols<br/></td>
+        <td><span>ShowRock</span><br/><br/><img src="/rock_singer5.png" alt="Slow dive" />Slow dive</td> 
+        </tr>
+        </table>
         </div>
+        
+        <div className="ques">
+            <h2>이 노래는 어떠신가요?</h2>
+            <h3>추천 플레이리스트 보기</h3>
+        </div>
+        <div class="musictable">
+    <table>
+        <tr>
+        <hr/><td>
+            <img src="/rock_m1.jpg" id="albumcover"/>
+            <div id="song">Owner of a Lonely Heart<br/><span>Yes</span></div>
+            <div class="play"><img src="sound1.png" id="m1"/><span>play</span></div></td>
+            <hr/>
         <hr/>
-        <div className="caution">
-            <h3>수많은 장르 중 하나의 장르를 선별하는 테스트이므로</h3>
-            <h3>개발자의 주관적인 판단, 생각이 들어있습니다.</h3>
-            <h3>결과는 언제나 시간에 따라 달라질 수 있으며</h3>
-            <h3>절대적이지 않음을 말씀드립니다.</h3>
+        </tr>
+        <tr>
+            <td><img src="/rock_m2.webp" id="albumcover"/>
+            <div id="song">Enter Sandman<br/><span>Metallica</span></div>
+            <div class="play"><img src="/sound1.png" id="m2"/><span>play</span></div>
+            </td><hr/>
+        </tr>
+        <tr>
+            <td><img src="/rock_m3.webp"  id="albumcover" />
+            <div id="song">Smells Like Teen Spirit<br/><span>Nirvana</span></div>
+            <div class="play"><img src="sound1.png" id="m3"/><span>play</span></div>
+            </td><hr/>
+        </tr>
+        <tr>
+            <td><img src="/rock_m4.jpg"  id="albumcover" />
+            <div id="song">God Save The Queen<br/><span>Sex pistols</span></div>
+            <div class="play"><img src="sound1.png" id="m4"/><span>play</span></div>
+            </td><hr/>
+        </tr>
+        <tr>
+            <td><img src="/rock_m5.jpg"  id="albumcover"/>
+            <div id="song">When the Sun Hits<br/><span>Slow dive</span></div>
+            <div class="play"><img src="sound1.png" id="m5"/><span>play</span></div>
+            </td><hr/> 
+        </tr>
+    </table>
+    </div>
+
+    <div class="graph">
+        <h2>현재 <span>나의 음악 DNA</span>분포도</h2>
         </div>
+        
         <VStack
             className="Stack"
             divider={<StackDivider borderColor='gray.300' />}
@@ -120,6 +166,11 @@ const Rock = () => {
                 </div>
             </Box>
         </VStack>
+        <div className="caution">
+            <h3>수많은 장르 중 하나의 장르를 선별하는 테스트이므로 개발자의 주관적인 판단, 생각이 들어있습니다.</h3>
+            <h3>결과는 언제나 시간에 따라 달라질 수 있으며 절대적이지 않음을 말씀드립니다.</h3>
+        </div>
+        </div>
         </ChakraProvider>
     );
 }
